a83cb8b586c785eb37ffbd17946e2b996825ab73
RBJEH <jeh@rainbow-robotics.com>
2025-07-14 16:30:09 +0900

    250714 software PLC ppt change

---
+++ assets/app.NjoLZNZf.js
@@ -0,0 +1,7 @@
+import{j as o,wQ as p,wR as u,wS as l,wT as c,wU as f,wV as d,wW as m,wX as w,wY as h,wZ as A,X as g,d as v,u as P,l as R,z as y,w_ as C,w$ as _,x0 as E,a8 as T}from"./chunks/framework.WRZeKJXX.js";import{R as b}from"./chunks/theme.li9bnraj.js";function i(e){if(e.extends){const t=i(e.extends);return{...t,...e,async enhanceApp(a){t.enhanceApp&&await t.enhanceApp(a),e.enhanceApp&&await e.enhanceApp(a)}}}return e}const s=i(b),D=v({name:"VitePressApp",setup(){const{site:e,lang:t,dir:a}=P();return R(()=>{y(()=>{document.documentElement.lang=t.value,document.documentElement.dir=a.value})}),e.value.router.prefetchLinks&&C(),_(),E(),s.setup&&s.setup(),()=>T(s.Layout)}});async function S(){const e=x(),t=j();t.provide(u,e);const a=l(e.route);return t.provide(c,a),t.component("Content",f),t.component("ClientOnly",d),Object.defineProperties(t.config.globalProperties,{$frontmatter:{get(){return a.frontmatter.value}},$params:{get(){return a.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:t,router:e,siteData:m}),{app:t,router:e,data:a}}function j(){return w(D)}function x(){let e=o,t;return h(a=>{let n=A(a),r=null;return n&&(e&&(t=n),(e||t===n)&&(n=n.replace(/\.js$/,".lean.js")),r=g(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&S().then(({app:e,router:t,data:a})=>{t.go().then(()=>{p(t.route,a.site),e.mount("#app")})});export{S as createApp};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
