22d4fc2465c0f718a2f58705501fd3578faaff63
yeajincho <yjcho@rainbow-robotics.com>
2026-04-30 15:43:27 +0900

    2026-04-30
    가반하중 20수정

---
+++ assets/README.md.UwKklGNX.lean.js
@@ -0,0 +1,1 @@
+import{_ as o,c as a,o as e,U as t}from"./chunks/framework.Zwu4-4z4.js";const k=JSON.parse('{"title":"cobot-docs","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),i={name:"README.md"},s=t("",24),l=[s];function n(r,d,h,c,p,b){return e(),a("div",null,l)}const _=o(i,[["render",n]]);export{k as __pageData,_ as default};
